8d4d73cb253d2f0a16a4760161d35fcd5fba3440
Konstantin Todorov <kz.todorov@gmail.com>
2012-03-26 23:12:08 +0300

    Removed scrolling when the arrow keys are pressed.

--- js/script.js
+++ js/script.js
@@ -419,14 +419,3 @@
 		document.addEventListener('keydown',onKeyDown);
 		document.addEventListener('keyup',onKeyUp);
 
-		var ar=new Array(33,34,35,36,37,38,39,40);
-		// function to disable the scrolling when pressing the arrow keys.
-		$(document).keydown(function(e) {
-		     var key = e.which;
-		      
-		      if($.inArray(key,ar) > -1) {
-		          e.preventDefault();
-		          return false;
-		      }
-		      return true;
-		});
